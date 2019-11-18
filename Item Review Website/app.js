0e8c2d70d84fcc76c173bf35b47a336f27b167a7
Jacob Hannigan (1000062029) <HANNJJ1@student.op.ac.nz>
2019-11-19 12:45:18 +1300

    Fixed login and added nav

--- Item Review Website/app.js
+++ Item Review Website/app.js
@@ -1,17 +1,20 @@
-const express = require('express')
 const fetch = require('node-fetch')
+const bodyParser = require('body-parser')
+const express = require('express')
 const expressLayouts = require('express-ejs-layouts')
 const path = require('path')
+const port = process.env.PORT || 3000
 const app = express()
 
-const port = process.env.PORT || 3000
-
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+)
+app.use(bodyParser.json())
 app.use(expressLayouts)
-
 app.set('view engine', 'ejs')
-
 app.set('views', path.join(__dirname, 'views'))
-
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', (req, res) => {
@@ -23,7 +26,13 @@ app.get('/login', (req, res) => {
 })
 
 app.post('/login', (req, res) => {
-    res.status(200).redirect('/login')
+    let userid = req.body.userid
+    let pwd = req.body.pwd
+    if (userid === "admin" && pwd === "admin"){
+        res.status(200).render('index')
+    }
+    else
+        res.status(200).redirect('/login')
 })
 
 // Listening to port 3000
