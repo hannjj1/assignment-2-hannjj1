0e8a9c4abd807aa65d8337bc407dae1285b9e79a
Jacob Hannigan (1000062029) <HANNJJ1@student.op.ac.nz>
2019-11-22 14:49:35 +1300

    Deployed to Heroku successfully

--- app.js
+++ app.js
@@ -17,38 +17,38 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/", (req, res) => {
+app.get("/", (req, res) => {                        //Default Page/Login Page
   res.status(200).render("login");
 });
 
-app.get("/home", (req, res) => {
+app.get("/home", (req, res) => {                    //Home Page
   res.status(200).render("home");
 });
 
-app.get("/contact", (req, res) => {
+app.get("/contact", (req, res) => {                 //Contact Page
   res.status(200).render("contact");
 });
 
-app.get("/cellphone", (req, res) => {
+app.get("/cellphone", (req, res) => {               //Cellphones page
   res.status(200).render("cellphone");
 });
 
-app.get("/camera", (req, res) => {
+app.get("/camera", (req, res) => {                  //Camera page
   res.status(200).render("camera");
 });
 
 
 
-app.get("*", function(req, res){
+app.get("*", function(req, res){                    //If page isn't any of the above, it will send you to the Error page.
   res.status(404).render('error')
 })
 
-app.post("/home", (req, res) => {
+app.post("/home", (req, res) => { 
   let userid = req.body.userid;
   let pwd = req.body.pwd;
-  if (userid === "user" && pwd === "P@ssw0rd123") {
-    res.status(200).render("home");
-  } else res.status(200).redirect("/");
+  if (userid === "user" && pwd === "P@ssw0rd123") { //Check if the Username and Password is correct
+    res.status(200).render("home");                 //If it is correct, it will take you to the home page.
+  } else res.status(200).redirect("/");             //Else it will refresh the page you are on
 });
 
 // Listening to port 3000
--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,3 @@
-var userName = document.getElementsByName("animal");
+var userName = document.getElementsByName("username");
 if (document.getElementById("button").clicked == true)
   alert("Thank you " + userName + ", we will get in touch soon");
