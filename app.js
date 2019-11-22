05f72d56e3f36753e8268dddc3b3697954b72a7b
Jacob Hannigan (1000062029) <HANNJJ1@student.op.ac.nz>
2019-11-22 13:50:13 +1300

    Fixed heroku not working

--- app.js
+++ app.js
@@ -1,68 +1,59 @@
-const fetch = require('node-fetch')
-const bodyParser = require('body-parser')
-const express = require('express')
-const expressLayouts = require('express-ejs-layouts')
-const path = require('path')
-const port = process.env.PORT || 3000
-const app = express()
+const fetch = require("node-fetch");
+const bodyParser = require("body-parser");
+const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
+const path = require("path");
+const port = process.env.PORT || 3000;
+const app = express();
 
 app.use(
   bodyParser.urlencoded({
     extended: true
   })
-)
-app.use(bodyParser.json())
-app.use(expressLayouts)
-app.set('view engine', 'ejs')
-app.set('views', path.join(__dirname, 'views'))
-app.use(express.static(path.join(__dirname, 'public')))
+);
+app.use(bodyParser.json());
+app.use(expressLayouts);
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.status(200).render("login");
+});
+
+app.get("/home", (req, res) => {
+  res.status(200).render("home");
+});
+
+app.get("/contact", (req, res) => {
+  res.status(200).render("contact");
+});
+
+app.get("/cellphone", (req, res) => {
+  res.status(200).render("cellphone");
+});
+
+app.get("/camera", (req, res) => {
+  res.status(200).render("camera");
+});
 
 
 
-app.get('/', (req, res) => {
-    res.status(200).render('login')
+app.get("*", function(req, res){
+  res.status(404).render('error')
 })
 
-app.get('/home', (req, res) => {
-    res.status(200).render('home')
-})
-
-app.get('/contact', (req, res) => {
-    res.status(200).render('contact')
-})
-
-app.get('/cellphone', (req, res) => {
-    res.status(200).render('cellphone')
-})
-
-app.get('/camera', (req, res) => {
-    res.status(200).render('camera')
-})
-
-/*
-app.use(
-    function(req, res){
-    res.status(404).render('error')
-
-app.use((req, res) => {
-    res.status(404).render('error')
-})
-})*/
-
-
-app.post('/home', (req, res) => {
-    let userid = req.body.userid
-    let pwd = req.body.pwd
-    if (userid === "user" && pwd === "P@ssw0rd123"){
-        res.status(200).render('home')
-    }
-    else
-        res.status(200).redirect('/')
-})
+app.post("/home", (req, res) => {
+  let userid = req.body.userid;
+  let pwd = req.body.pwd;
+  if (userid === "user" && pwd === "P@ssw0rd123") {
+    res.status(200).render("home");
+  } else res.status(200).redirect("/");
+});
 
 // Listening to port 3000
 app.listen(port, _ => {
-    console.log(`Listening on port ${port}`)
-})
+  console.log(`Listening on port ${port}`);
+});
 
-module.exports = app 
+module.exports = app;
--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,3 @@
-var userName = document.getElementsByName("animal")
-if(document.getElementById('button').clicked == true)
-    alert("Thank you " + userName + ", we will get in touch soon");
+var userName = document.getElementsByName("animal");
+if (document.getElementById("button").clicked == true)
+  alert("Thank you " + userName + ", we will get in touch soon");
